73941384ca6bff1500aeb3b0f39266dd73f8e156
RadheKrishnaBivu <rasa@RadheKrishnaBivus-MacBook-Air.local>
2024-08-02 15:03:39 +0530

    stage 4

--- components/A.tsx
+++ components/A.tsx
@@ -7,17 +7,13 @@ export default function A(){
       sequence={[
         // Same substring at the start will only be typed out once, initially
         "Let's share love",
-        1000, // wait 1s before replacing "Mice" with "Hamsters"
-        "Let's share knowledge",
-        1000,
-        "Let's share awareness",
-        1000,
+        1000, // wait 1s before replacing "Mice" with "Hamsters
         "Let's create mind",
         1000
       ]}
       wrapper="span"
-      speed={70}
-      style={{ fontSize: '1.7em', color: 'red', display: 'inline-block' }}
+      speed={60}
+      style={{ fontSize: '1.4em', color: 'red', display: 'inline-block' }}
       repeat={Infinity}
     />
         </div>
